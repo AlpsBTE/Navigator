b2bfe21af1a118db646b630538124d9972485c36
LordTuxn <r3tuxn@gmail.com>
2021-08-17 23:47:26 +0200

    Updated and improved PortalManager

--- src/main/java/io/github/AlpsBTE_Navigator/utils/PortalManager.java
+++ src/main/java/io/github/AlpsBTE_Navigator/utils/PortalManager.java
@@ -3,15 +3,17 @@ package github.AlpsBTE_Navigator.utils;
 import com.sk89q.worldedit.Vector;
 import com.sk89q.worldedit.regions.CuboidRegion;
 import com.sk89q.worldedit.regions.Region;
-import github.AlpsBTE_Navigator.AlpsBTE_Navigator;
+import github.AlpsBTE_Navigator.NavigatorPlugin;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.World;
-import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import xyz.xenondevs.particle.ParticleBuilder;
 import xyz.xenondevs.particle.ParticleEffect;
+import xyz.xenondevs.particle.task.TaskManager;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 
 public class PortalManager extends Thread {
@@ -23,13 +25,13 @@ public class PortalManager extends Thread {
     public Region Portal_Event = new CuboidRegion(Vector.toBlockPoint(540, 79, 544), Vector.toBlockPoint(542, 79, 546));
 
     public void run() {
-        Bukkit.getScheduler().scheduleSyncRepeatingTask(AlpsBTE_Navigator.getPlugin(), () -> {
-            portalWorld = Utils.getSpawnPoint().getWorld();
+        portalWorld = Utils.getSpawnPoint().getWorld();
 
-            spawnPlotParticles(Portal_Plot.getMinimumPoint(), Portal_Plot.getMaximumPoint());
-            spawnPlotParticles(Portal_Terra.getMinimumPoint(), Portal_Terra.getMaximumPoint());
-            spawnPlotParticles(Portal_Event.getMinimumPoint(), Portal_Event.getMaximumPoint());
+        spawnPortalParticles(Portal_Plot.getMinimumPoint(), Portal_Plot.getMaximumPoint());
+        spawnPortalParticles(Portal_Terra.getMinimumPoint(), Portal_Terra.getMaximumPoint());
+        spawnPortalParticles(Portal_Event.getMinimumPoint(), Portal_Event.getMaximumPoint());
 
+        Bukkit.getScheduler().scheduleSyncRepeatingTask(NavigatorPlugin.getPlugin(), () -> {
             for (Player player : Bukkit.getOnlinePlayers()) {
                 try {
                     Vector playerLocation = Vector.toBlockPoint(player.getLocation().getX(), player.getLocation().getY(), player.getLocation().getZ());
@@ -37,11 +39,11 @@ public class PortalManager extends Thread {
                         player.teleport(Utils.getSpawnPoint());
                         player.performCommand("companion");
                     } else if (Portal_Terra.contains(playerLocation)) {
-                        AlpsBTE_Navigator.getPlugin().connectPlayer(player, Utils.TERRA_SERVER);
+                        NavigatorPlugin.getPlugin().connectPlayer(player, Utils.TERRA_SERVER);
                     } else if (Portal_Event.contains(playerLocation)) {
-                        if (AlpsBTE_Navigator.getPlugin().getConfig().getBoolean("servers.event.joinable") || player.hasPermission("alpsbte.joinEventStaff")) {
+                        if (NavigatorPlugin.getPlugin().getConfig().getBoolean("servers.event.joinable") || player.hasPermission("alpsbte.joinEventStaff")) {
                             if (player.hasPermission("alpsbte.joinEvent")) {
-                                AlpsBTE_Navigator.getPlugin().connectPlayer(player, Utils.EVENT_SERVER);
+                                NavigatorPlugin.getPlugin().connectPlayer(player, Utils.EVENT_SERVER);
                             }
                         }
                     }
@@ -52,18 +54,16 @@ public class PortalManager extends Thread {
         }, 1, 15);
     }
 
-    public void spawnPlotParticles(Vector min, Vector max) {
-        for (int i = min.getBlockX(); i <= max.getBlockX();i++) {
+    public void spawnPortalParticles(Vector min, Vector max) {
+        List<Object> packets = new ArrayList<>();
+        ParticleBuilder particle = new ParticleBuilder(ParticleEffect.CLOUD).setOffsetX(0.5f).setOffsetZ(0.5f).setSpeed(0.05f);
+        for (int i = min.getBlockX(); i <= max.getBlockX(); i++) {
             for (int j = min.getBlockY(); j <= max.getBlockY(); j++) {
-                for (int k = min.getBlockZ(); k <= max.getBlockZ();k++) {
-                    new ParticleBuilder(ParticleEffect.CLOUD, new Location(portalWorld, i, j, k))
-                            .setOffsetX(0.5f)
-                            .setOffsetZ(0.5f)
-                            .setSpeed(0.05f)
-                            .display();
+                for (int k = min.getBlockZ(); k <= max.getBlockZ(); k++) {
+                    packets.add(particle.setLocation(new Location(portalWorld, i, j, k)).toPacket());
                 }
             }
         }
+        TaskManager.startGlobalTask(packets, 2);
     }
-
 }
