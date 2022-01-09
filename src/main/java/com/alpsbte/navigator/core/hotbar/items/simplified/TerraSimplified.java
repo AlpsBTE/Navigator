f6b3c38d817e24e4d5be79ae54654de9fe9fe826
LordTuxn <r3tuxn@gmail.com>
2022-01-09 20:05:40 +0100

    add a simplified version of terra and plot item

---
+++ src/main/java/com/alpsbte/navigator/core/hotbar/items/simplified/TerraSimplified.java
@@ -0,0 +1,52 @@
+package com.alpsbte.navigator.core.hotbar.items.simplified;
+
+import com.alpsbte.navigator.core.hotbar.items.Terra;
+import com.alpsbte.navigator.utils.ItemBuilder;
+import com.alpsbte.navigator.utils.LoreBuilder;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class TerraSimplified extends Terra {
+
+    @Override
+    public Material getMaterial() {
+        return Material.EYE_OF_ENDER;
+    }
+
+    @Override
+    public String getTitle() {
+        return "§b§lVISIT US §7(Right Click)";
+    }
+
+    @Override
+    public List<String> getDescription() {
+        return Arrays.asList("Discover all three countries by visiting already",
+                "built cities, villages and famous places.");
+    }
+
+    @Override
+    public List<String> getFeatures() {
+        return null;
+    }
+
+    @Override
+    public ItemStack createItem() {
+        return new ItemBuilder(getMaterial(), 1)
+                .setName(getTitle())
+                .setLore(new LoreBuilder()
+                        .description(getDescription())
+                        .emptyLine()
+                        .features(getFeatures())
+                        .emptyLine()
+                        .version(getVersion(), isModded())
+                        .build())
+                .build();
+    }
+
+    public static ItemStack getItem() {
+        return new TerraSimplified().createItem();
+    }
+}
