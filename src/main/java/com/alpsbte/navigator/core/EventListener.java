3a7d47aef4719567b73a2988a9f1b7f68b0c1dcd
LordTuxn <r3tuxn@gmail.com>
2022-01-09 20:06:49 +0100

    add new items to player's hotbar

--- src/main/java/com/alpsbte/navigator/core/EventListener.java
+++ src/main/java/com/alpsbte/navigator/core/EventListener.java
@@ -3,19 +3,19 @@ package com.alpsbte.navigator.core;
 import com.alpsbte.navigator.NavigatorPlugin;
 import com.alpsbte.navigator.core.config.ConfigPaths;
 import com.alpsbte.navigator.core.hotbar.NavigatorMenu;
+import com.alpsbte.navigator.core.hotbar.items.simplified.PlotSimplified;
+import com.alpsbte.navigator.core.hotbar.items.simplified.TerraSimplified;
 import com.alpsbte.navigator.utils.Utils;
 import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
-
-import java.util.List;
-import java.util.UUID;
-import java.util.logging.Level;
+import org.bukkit.inventory.ItemStack;
 
 public class EventListener implements Listener {
 
@@ -26,8 +26,20 @@ public class EventListener implements Listener {
         Bukkit.getScheduler().runTaskAsynchronously(NavigatorPlugin.getPlugin(), () -> {
             FileConfiguration config = NavigatorPlugin.getPlugin().getConfig();
 
-            if(!event.getPlayer().getInventory().contains(NavigatorMenu.getItem())) {
+            if (NavigatorPlugin.getLuckPerms() != null && !event.getPlayer().hasPermission("group.builder")) {
+                ItemStack terraSimplifiedItem = TerraSimplified.getItem();
+                ItemStack plotSimplifiedItem = PlotSimplified.getItem();
+
+                if (!event.getPlayer().getInventory().contains(terraSimplifiedItem)) {
+                    event.getPlayer().getInventory().setItem(0, terraSimplifiedItem);
+                }
+
+                if (!event.getPlayer().getInventory().contains(plotSimplifiedItem)) {
+                    event.getPlayer().getInventory().setItem(1, plotSimplifiedItem);
+                }
+            } else if(!event.getPlayer().getInventory().contains(NavigatorMenu.getItem())) {
                 event.getPlayer().getInventory().setItem(0, NavigatorMenu.getItem());
+                event.getPlayer().getInventory().setItem(1, null);
             }
 
             if(config.getBoolean(ConfigPaths.SERVERS_PLOT_ENABLE_JOIN_MESSAGE)) {
@@ -50,23 +62,52 @@ public class EventListener implements Listener {
                         NavigatorPlugin.getPlugin().getLogger().log(Level.SEVERE, "Could not add builder permission to player: " + builder, ex);
                     }
                 }
-            }
+            }*/
         });
     }
 
     @EventHandler
     public void onPlayerClickEvent(PlayerInteractEvent event) {
         if(event.getAction().equals(Action.RIGHT_CLICK_BLOCK) || event.getAction().equals(Action.RIGHT_CLICK_AIR)) {
-            if(event.getItem() != null && event.getItem().equals(NavigatorMenu.getItem())) {
-                NavigatorPlugin.getPlugin().UpdatePlayerCount(event.getPlayer());
+            if(event.getItem() != null) {
+                if (event.getItem().equals(NavigatorMenu.getItem())) {
+                    NavigatorPlugin.getPlugin().UpdatePlayerCount(event.getPlayer());
+                } else {
+                    TerraSimplified terra = new TerraSimplified();
+                    if (event.getItem().equals(TerraSimplified.getItem())) {
+                        if (terra.serverIsOnline) {
+                            event.getPlayer().sendMessage(Utils.getInfoMessageFormat("Connecting to server"));
+                            NavigatorPlugin.getPlugin().connectPlayer(event.getPlayer(), "ALPS-2");
+                        } else {
+                            event.getPlayer().sendMessage(Utils.getErrorMessageFormat("Server is offline"));
+                        }
+                    } else {
+                        if (event.getItem().equals(PlotSimplified.getItem())) {
+                            event.getPlayer().performCommand("companion");
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    @EventHandler
+    public void onInventoryClickEvent(InventoryClickEvent event) {
+        int slot = event.getSlot();
+        ItemStack item = event.getWhoClicked().getInventory().getItem(slot);
+        if (item != null) {
+            if (item.equals(NavigatorMenu.getItem()) || item.equals(TerraSimplified.getItem()) || item.equals(PlotSimplified.getItem())) {
+                event.setCancelled(true);
             }
         }
     }
 
     @EventHandler
     public void onPlayerItemDropEvent(PlayerDropItemEvent event) {
-        if(event.getItemDrop() != null && event.getItemDrop().getItemStack().equals(NavigatorMenu.getItem())) {
-            event.setCancelled(true);
+        if(event.getItemDrop() != null) {
+            if (event.getItemDrop().getItemStack().equals(NavigatorMenu.getItem()) || event.getItemDrop().getItemStack().equals(PlotSimplified.getItem()) || event.getItemDrop().getItemStack().equals(TerraSimplified.getItem())) {
+                event.setCancelled(true);
+            }
         }
     }
 }
